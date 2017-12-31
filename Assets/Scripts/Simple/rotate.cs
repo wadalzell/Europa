911eb8ee1d314a784bc8994fd68bd9806e877dd2
wadalzell <wadalzell@gmail.com>
2017-12-31 07:46:52 -0800

    Cloud Update

--- Assets/Scripts/Simple/rotate.cs
+++ Assets/Scripts/Simple/rotate.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour {
 
-	public float rotSpeed = 1;
+	public float rotSpeed = 1;	
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate (0, rotSpeed, 0);
